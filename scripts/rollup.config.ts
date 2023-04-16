ea22b872de7acdb50de99af0994244235919f5a5
Shopping <xpzheng6@163.com>
2023-04-16 15:19:44 +0800

    refactor: build

--- scripts/rollup.config.ts
+++ scripts/rollup.config.ts
@@ -1,20 +1,29 @@
 import path from 'path'
 import { defineConfig } from 'rollup'
 import alias from '@rollup/plugin-alias'
+import { babel } from '@rollup/plugin-babel'
 import sizes from '@atomico/rollup-plugin-sizes'
 import ts from '@rollup/plugin-typescript'
 import beep from '@rollup/plugin-beep'
 import { terser } from 'rollup-plugin-terser'
-import pkg from './package.json'
+import pkg from '../package.json'
 import clear from 'rollup-plugin-clear'
 
 export default defineConfig({
   input: 'src/index.ts',
-  output: {
+  output: [{
     dir: 'dist',
     format: 'cjs',
-  },
-  external: Object.keys((pkg as any).peerDependencies || {}),
+    entryFileNames: '[name].cjs'
+  }, {
+    dir: 'dist',
+    format: 'esm',
+    entryFileNames: '[name].mjs'
+  }],
+  external: [
+    'path',
+    'fs',
+  ],
   plugins: [
     alias({
       entries: [
@@ -28,7 +37,10 @@ export default defineConfig({
       targets: ['dist'],
     }),
     ts({
-      tsconfig: path.resolve(__dirname, './tsconfig.build.json'),
+      tsconfig: path.resolve(__dirname, '../tsconfig.json'),
+    }),
+    babel({
+      babelHelpers: 'runtime',
     }),
     // 生成包大小监控
     sizes(100),
